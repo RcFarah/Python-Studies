dbbd5fc0094a1962ee3c128fe85cfd4d8baed0d9
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-03-24 18:08:14 -0300

    Update Exercicio#042.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#042.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#042.py
@@ -4,8 +4,7 @@ seg_a = int(input('Digite o valor do segmento a: '))
 seg_b = int(input('Digite o valor do segmento b: '))
 seg_c = int(input('Digite o valor do segmento c: '))
 
-if (seg_b - seg_c) < seg_a < seg_b + seg_c and (seg_a - seg_c) < seg_b < seg_a + seg_c and (seg_a - seg_b) < seg_c < \
-        seg_a + seg_b:
+if seg_a < seg_b + seg_c and seg_b < seg_a + seg_c and seg_c < seg_a + seg_b:
     print('Esses segmentos podem formar um triângulo!')
     if seg_a == seg_b == seg_c:
         print('Esse é um triangulo EQUILÁTERO!')
