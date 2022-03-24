5d7a50dee3c08b9661fd8a292e192431a29b0a22
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-03-24 18:08:45 -0300

    Update Exercicio#035.py

--- Curso em Video - Mundo 1/Exercicios/Exercicio#035.py
+++ Curso em Video - Mundo 1/Exercicios/Exercicio#035.py
@@ -4,8 +4,7 @@ segmento_a = float(input('Coloque o valor de um lado: '))
 segmento_b = float(input('Coloque o valor de outro lado: '))
 segmento_c = float(input('Coloque o valor de outro lado: '))
 
-if (segmento_b - segmento_c) < segmento_a < segmento_b + segmento_c and (segmento_a - segmento_c) < segmento_b < \
-        segmento_a + segmento_c and (segmento_a - segmento_b) < segmento_c < segmento_a + segmento_b:
+if segmento_a < segmento_b + segmento_c and segmento_b < segmento_a + segmento_c and segmento_c < segmento_a + segmento_b:
     print('Os lados {}, {} e {} podem formar triângulo.'.format(segmento_a, segmento_b, segmento_c))
 else:
     print('Os lados {}, {} e {} não podem formar triângulo.'.format(segmento_a, segmento_b, segmento_c))
