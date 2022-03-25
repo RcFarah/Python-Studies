b283ee1dbbb99d69b9db58e44651b3f907a8564f
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-03-24 23:26:07 -0300

    Update Exercicio#043.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#043.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#043.py
@@ -6,12 +6,17 @@ altura_metro = altura / 100
 calculo_imc = peso / (altura_metro ** 2)
 
 if calculo_imc < 18.5:
-    print('Abaixo do Peso.')
+    print('Abaixo do Peso.\n'
+          'Seu IMC é de: {:.2f}'.format(calculo_imc))
 elif 18.5 < calculo_imc < 25:
-    print('Peso Ideal!')
+    print('Peso Ideal!\n'
+          'Seu IMC é de: {:.2f}'.format(calculo_imc))
 elif 25 <= calculo_imc < 30:
-    print('Sobrepeso.')
+    print('Sobrepeso.\n'
+          'Seu IMC é de: {:.2f}'.format(calculo_imc))
 elif 30 <= calculo_imc <= 40:
-    print('Obesidade.')
+    print('Obesidade.\n'
+          'Seu IMC é de: {:.2f}'.format(calculo_imc))
 elif calculo_imc > 40:
-    print('Obesidade Mórbida.')
+    print('Obesidade Mórbida.\n'
+          'Seu IMC é de: {:.2f}'.format(calculo_imc))
