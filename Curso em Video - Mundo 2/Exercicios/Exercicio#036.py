b5e8def58d47a0536da806fef0135c64d19d0c69
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-04-03 11:29:00 -0300

    Update Exercicio#036.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#036.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#036.py
@@ -1,5 +1,9 @@
 # Desafio 036
 
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa,
+# o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então
+# o empréstimo será negado.
+
 from time import sleep
 from sys import exit
 
