af92926e18f72f95a9891b70c02bfb670bfd42d6
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-04-03 11:28:42 -0300

    Update Exercicio#037.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#037.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#037.py
@@ -1,5 +1,8 @@
 # Exercicio #037
 
+# Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base
+# de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+
 numero = int(input('Digite um número inteiro: '))
 
 opcao = int(input('Para qual base deseja converter?\n'
