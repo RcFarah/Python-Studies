4d088f42457166e17514b9686205a4319a30fc27
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-04-03 11:26:17 -0300

    Update Exercicio#038.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#038.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#038.py
@@ -1,5 +1,10 @@
 # Exercício #038
 
+# Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+# - O primeiro valor é maior
+# - O segundo valor é maior
+# - Não existe valor maior, os dois são iguais
+
 numero1 = int(input('Digite o valor do primeiro número: '))
 numero2 = int(input('Digite o valor do segundo número: '))
 
