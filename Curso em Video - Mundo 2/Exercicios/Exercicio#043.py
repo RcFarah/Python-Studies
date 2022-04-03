a69cadec5bf791369ec17f69766ca8b2437f6550
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-04-03 11:24:36 -0300

    Update Exercicio#043.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#043.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#043.py
@@ -1,5 +1,13 @@
 # Exercicio #043
 
+#  Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e
+#  mostre seu status, de acordo com a tabela abaixo:
+# - IMC abaixo de 18,5: Abaixo do Peso
+# - Entre 18,5 e 25: Peso Ideal
+# - 25 até 30: Sobrepeso
+# - 30 até 40: Obesidade
+# - Acima de 40: Obesidade Mórbida
+
 altura = int(input('Digite a sua alutra (em cm): '))
 peso = int(input('Digite o seu peso (em KG): '))
 altura_metro = altura / 100
