a4bf466ea3b95fa92cae70fbf2ae5f4afb7689a7
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-04-03 11:25:27 -0300

    Update Exercicio#040.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#040.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#040.py
@@ -1,5 +1,11 @@
 # Exercício #040
 
+# Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com
+# a média atingida:
+# - Média abaixo de 5.0: REPROVADO
+# - Média entre 5.0 e 6.9: RECUPERAÇÃO
+# - Média 7.0 ou superior: APROVADO
+
 aluno = str(input('Digite o nome do Aluno: ')).title().strip()
 nota1 = int(input('Digite a primeira nota de {}: '.format(aluno)))
 nota2 = int(input('Digite a segunda nota de {}: '.format(aluno)))
