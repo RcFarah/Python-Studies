23fbdf41a6f2eb0c919905b3b57886df57e29337
Rodrigo Farah <64575763+RcFarah@users.noreply.github.com>
2022-04-03 11:23:54 -0300

    Update Exercicio#044.py

--- Curso em Video - Mundo 2/Exercicios/Exercicio#044.py
+++ Curso em Video - Mundo 2/Exercicios/Exercicio#044.py
@@ -1,5 +1,12 @@
 # Exercicio #044
 
+# Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição
+# de pagamento:
+# - à vista dinheiro/cheque: 10% de desconto
+# - à vista no cartão: 5% de desconto
+# - em até 2x no cartão: preço formal
+# - 3x ou mais no cartão: 20% de juros
+
 print('{:-^40}'.format(' LOJA DE AÇUDE '))  # O ^ serve para indicar que é para preencher o que se pede dentro de X
 # caracteres
 
